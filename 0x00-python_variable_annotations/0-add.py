a0c9cc018ec07e460224c2cb3d6b422f067e7f90
TSOlami <tijani.saheed@yahoo.com>
2023-08-04 17:26:55 +0100

    Fix module documentations for task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-""" A type-annotated function add that takes a float a
-and a float b as arguments
-and returns their sum as a float """
+""" A type-annotated function add that takes a and a b as arguments"""
 
 
 def add(a: float, b: float) -> float:
