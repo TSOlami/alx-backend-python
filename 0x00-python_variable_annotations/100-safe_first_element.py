b91ead010e38cea97ddeae6de619829f5bc0480e
TSOlami <tijani.saheed@yahoo.com>
2023-08-04 18:54:16 +0100

    Properly documenting task 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,7 @@ from typing import Any, Union, Sequence
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    # The types of the elements of the input are not know
+    """The types of the elements of the input are not know"""
     if lst:
         return lst[0]
     else:
