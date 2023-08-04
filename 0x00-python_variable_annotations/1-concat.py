d8c4a92917a974dfa3f54758b439b99957cb4d1a
TSOlami <tijani.saheed@yahoo.com>
2023-08-04 17:37:22 +0100

    Fix module documentations for task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,4 +2,5 @@
 """Type-annotated function concat that takes a string str1 and a string str2"""
 
 def concat(str1: str, str2: str) -> str:
+    """ Concatenated two string"""
     return "{}{}".format(str1, str2)
