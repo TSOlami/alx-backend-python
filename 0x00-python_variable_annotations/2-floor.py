d099913da843b640e3a13ca2de81b8fa25159715
TSOlami <tijani.saheed@yahoo.com>
2023-08-04 17:13:09 +0100

    add the solutions for task 2 and 3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Returns floor and its type"""
+
+import math
+
+
+def floor(n: float) -> int:
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" converts a float to a string"""
+
+
+def to_str(n: float) -> str:
+    return str(n)
