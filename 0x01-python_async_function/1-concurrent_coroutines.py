884f7cf7e1ebc93be08d818d56706e94802c73cd
TSOlami <tijani.saheed@yahoo.com>
2023-08-07 22:17:44 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Execute multiple coroutines at the same time with async """
+""" An async routine called wait_n that takes in 2 int arguments """
 
 import asyncio
 from typing import List
@@ -8,10 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    2 int arguments (in this order): n and max_delay
-    async routine called wait_n
-    """
+    """ Spawn wait_random n times with the specified max_delay """
 
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
