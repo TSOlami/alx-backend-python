d0409734f0d996364a8799026d160692a3824e39
TSOlami <tijani.saheed@yahoo.com>
2023-08-09 02:46:52 +0100

    fix task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""measure_runtime should measure the total runtime and return it."""
+""" Import from task 1 and write a measure_runtime coroutine """
 import time
 import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
@@ -7,8 +7,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """
-    measure_runtime should measure the total runtime and return it.
-    four times in parallel using asyncio.gather.
+    Execute async_comprehension four times in parallel using asyncio.gather.
     """
     t_start = time.perf_counter()
     task = [async_comprehension() for i in range(4)]
