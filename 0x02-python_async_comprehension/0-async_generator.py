53c5f0134845aaf28735ec85393845dd7295c24f
TSOlami <tijani.saheed@yahoo.com>
2023-08-09 02:35:38 +0100

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Fuction called async_generator that takes no arguments."""
+""" Function called async_generator that takes no arguments."""
 import random
 import asyncio
 from typing import Generator
