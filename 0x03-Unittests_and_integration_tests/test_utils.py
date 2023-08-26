1f16257268b1c9527942fd0f9de4f00cbc8f6e45
TSOlami <tijani.saheed@yahoo.com>
2023-08-26 16:27:19 +0100

    attempt task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
+"""Write the unit test for utils.access_nested_map.
+
+Create a TestAccessNestedMap class that inherits from
+unittest.TestCase.
+
+Implement the TestAccessNestedMap.test_access_nested_map
+method to test that the method returns what it is supposed to.
+
+Decorate the method with @parameterized.expand to test the function
+"""
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """Class that inherits from unittest.TestCase.
+    """
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -24,4 +37,23 @@ class TestAccessNestedMap(unittest.TestCase):
             self, nested_map, path, expected_exception):
         """Unit test method for testing access_nested_map module exception
         """
-        self.assertRaises(expected_exception, access_nested_map, nested_map, path)
+        self.assertRaises(
+            expected_exception, access_nested_map, nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Class to implement the TestGetJson.test_get_json method
+    """
+
+    @parameterized.expand([
+        ('http://example.com', {'payload': True}),
+        ('http://holberton.io', {'payload': False})
+        ])
+    def test_get_json(self, url, expected_result):
+        """Method to test that utils.get_json returns the expected result.
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = expected_result
+        with patch('requests.get', return_value=mock_response):
+            response = get_json(url)
+            self.assertEqual(response, expected_result)
