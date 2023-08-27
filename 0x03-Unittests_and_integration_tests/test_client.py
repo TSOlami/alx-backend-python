b96dbac5dc8015faae8a6ebc0b599df9b2465fa5
TSOlami <tijani.saheed@yahoo.com>
2023-08-27 08:57:45 +0100

    Remove whitespaces

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -17,26 +17,26 @@ from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    
+
     """A Github org client test object"""
     @parameterized.expand([
         ("google", {'login': "google"}),
         ("abc", {'login': "abc"}),
-	])
+    ])
     @patch(
-	    "client.get_json",
-	)
+        "client.get_json",
+    )
     def test_org(self, org: str, expected_response: Dict,
                  mocked_function: MagicMock):
         """Implements the test_org method.
         """
         mocked_function.return_value = MagicMock(
             return_value=expected_response)
-        
+
         gitclient = GithubOrgClient(org)
-        
+
         self.assertEqual(gitclient.org(), expected_response)
-        
+
         mocked_function.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
