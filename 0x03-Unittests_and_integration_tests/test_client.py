3dc63efa4489f2af1998a81fa7f774abfff52aeb
TSOlami <tijani.saheed@yahoo.com>
2023-08-27 08:54:53 +0100

    Test a github org client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python3
 import unittest
 from unittest.mock import patch
-from parameterized import parameterized
+from parameterized import (parameterized, parameterized_class)
 from client import GithubOrgClient
+from typing import (
+    List,
+    Dict,
+)
+from unittest.mock import (
+    patch,
+    MagicMock,
+    Mock,
+    PropertyMock,
+)
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    def test_org(self):
+    
+    """A Github org client test object"""
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+	])
+    @patch(
+	    "client.get_json",
+	)
+    def test_org(self, org: str, expected_response: Dict,
+                 mocked_function: MagicMock):
+        """Implements the test_org method.
+        """
+        mocked_function.return_value = MagicMock(
+            return_value=expected_response)
         
-        pass
+        gitclient = GithubOrgClient(org)
+        
+        self.assertEqual(gitclient.org(), expected_response)
+        
+        mocked_function.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
